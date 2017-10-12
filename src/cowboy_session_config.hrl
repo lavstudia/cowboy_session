29a74a906aff6df6380ceeba9064c4f5b1c564ba
dronowar <dronowar@gmail.com>
2017-10-13 01:49:02 +0300

    simple config

---
+++ src/cowboy_session_config.hrl
@@ -0,0 +1,30 @@
+%% ===================================================================
+%% Common project options
+%% ===================================================================
+
+-define(CHILD(I, Type),
+     #{id => I,
+        start => {I, start_link, []},
+        restart => permanent,
+        shutdown => 5000,
+        type => Type,
+        modules => [I]
+    }).
+-define(CHILD(I, Type, Restart),
+     #{id => I,
+        start => {I, start_link, []},
+        restart => Restart,
+        shutdown => 5000,
+        type => Type,
+        modules => [I]
+    }).
+
+
+%% ===================================================================
+%% Cookies options
+%% ===================================================================
+
+-define(C_NAME, <<"session">>).
+-define(C_OPTIONS, [{path, <<"/">>}]).
+-define(C_EXPIRE, 1440).
+-define(C_STORAGE, cowboy_session_storage_ets).
